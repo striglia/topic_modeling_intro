a052c059674ca4cdba46cf69cf232fee8e21e9fa
Scott Triglia <scott.triglia@gmail.com>
2014-08-26 15:50:05 -0700

    Initial version of section 2

---
+++ section_2/README.md
@@ -0,0 +1,20 @@
+Section 2: Exploration!
+------------------------
+
+
+Our goals for this section is to:
+
+1. Use your own code from the last section or (if you prefer or did not finish) this section's pre-built code.
+2. Explore the first few businesses in the business_reviews list and think about how you'd categorize their content. What Yelp categories belong? Can you come up wtih any appropriate non-yelp categories?
+3. Implement your version of `top_five_interesting_words` function and explore a few businesses with it. (hint: we don't care about words like "the"!)
+
+
+All code can be found in this directory. Look at the starter methods in
+`explore.py`. The comments should point you in the right direction, but if
+you get stuck feel free to ask me or a neighbor for help.
+
+If you're already done with this and waiting for the next section, try
+writing a classifier for identifying whether a business has the `Mexican`
+category! In the next section we'll be working a lot with this classifier, so
+start to think about what words are important and how to tell whether your
+classifier works well.
---
+++ section_2/clean_data.py
@@ -0,0 +1,51 @@
+"""
+A starter script for reading our Yelp data and cleaning up the reviews.
+"""
+import gzip
+import json
+import os
+
+
+def main():
+    project_root_directory = os.path.join(os.path.dirname(__file__), '../')
+    data_file = os.path.join(project_root_directory, 'sample_data.json.gz')
+    print 'The data file path is {0}'.format(data_file)
+
+    business_reviews = read_data_file(data_file)
+    sample_business = business_reviews[0]
+
+    # Let's look at the first business just to get an idea of what we're
+    # working with...
+    print sample_business['reviews'][0]
+    print sample_business['categories']
+    print sample_business['name']
+
+
+def clean_review(review):
+    """Removes simple stopwords, lowercases, fixes newlines"""
+    review = review.lower()
+    return review
+
+
+def clean_biz(biz):
+    """Clean up a business's text data so we can do further processing."""
+    return {
+        'name': biz['name'],
+        'id': biz['business_id'],
+        'categories': biz['categories'],
+        'reviews': [clean_review(r) for r in biz['reviews']],
+    }
+
+
+def read_data_file(data_file):
+    """Take a filepath and load the business review data."""
+    with gzip.open(data_file, 'r') as input_file:
+        data = [json.loads(line) for line in input_file]
+
+    # For each business, do basic data cleanup.
+    return [clean_biz(biz) for biz in data]
+
+
+if __name__ == '__main__':
+    """This gets called when we run this script!"""
+    main()
